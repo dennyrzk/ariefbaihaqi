fcbe5f8b3f39aa8cc1815486f49070d90acb79b0
dennyrzk <dannyrizky058@gmail.com>
2025-01-12 16:11:59 +0700

    Add files via upload

---
+++ live codding.py
@@ -0,0 +1,5 @@
+harga_ayam = [
+    {"jenis": "krispi", "ukuran": {"kecil": 10000, "sedang": 15000, "besar": 20000 }},
+    {"jenis": "original", "ukuran": {"kecil": 9000, "sedang": 14000, "besar": 19000}},
+    {"jenis": "pedas", "ukuran": {"kecil": 11000, "sedang": 10}}
+]
